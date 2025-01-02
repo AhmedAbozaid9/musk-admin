c15073e496baeb7499ab28b57f7f3c840a59dc5b
Ahmed <ahmedabozaid.dev@gmail.com>
2025-01-02 19:40:18 +0200

    adds route protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import {
-  Navigate,
   Route,
   BrowserRouter as Router,
   Routes,
+  useNavigate,
 } from "react-router-dom";
 import Layout from "./components/general/Layout";
 import BrandsPage from "./pages/BrandsPage";
@@ -12,21 +12,52 @@ import OrdersPage from "./pages/OrdersPage";
 import SettingsPage from "./pages/SettingsPage";
 import UsersPage from "./pages/UsersPage";
 
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import Loading from "./components/general/Loading";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/categories" />} />
-          <Route path="categories" element={<CategoriesPage />} />
-          <Route path="brands" element={<BrandsPage />} />
-          <Route path="coupons" element={<CouponsPage />} />
-          <Route path="users" element={<UsersPage />} />
-          <Route path="orders" element={<OrdersPage />} />
-          <Route path="settings" element={<SettingsPage />} />
-        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/*" element={<ProtectedRoutes />} />
       </Routes>
     </Router>
+  );
+}
+
+function ProtectedRoutes() {
+  const token = Cookies.get("musc-admin-token");
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+      setLoading(false);
+    } else {
+      setLoading(false);
+    }
+  }, [token]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="categories" element={<CategoriesPage />} />
+        <Route path="brands" element={<BrandsPage />} />
+        <Route path="coupons" element={<CouponsPage />} />
+        <Route path="users" element={<UsersPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="settings" element={<SettingsPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/general/Loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-pulse">
+          <h3 className="font-semibold text-4xl">Musc</h3>
+        </div>
+      </div>
+    </>
+  );
+}
