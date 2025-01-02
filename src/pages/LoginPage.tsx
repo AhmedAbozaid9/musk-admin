244b28faf83e5b8001b26f7a162efa244f9bab5c
Ahmed <ahmedabozaid.dev@gmail.com>
2025-01-02 19:48:56 +0200

    adds login layout

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,15 @@
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className=" shadow-lg">
+        <div className="rounded-t-2xl  text-center bg-black text-white px-32 py-8">
+          <h1 className="text-lg font-semibold">Musc</h1>
+          <h2 className="text-3xl font-semibold">نسجيل الدخول</h2>
+        </div>
+        <form className="px-16 py-8"></form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
