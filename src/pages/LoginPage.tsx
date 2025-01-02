6f2c8c7f4140aa00c95f30964fd496e37a6e8a2d
Ahmed <ahmedabozaid.dev@gmail.com>
2025-01-02 20:00:49 +0200

    adds sign in form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className=" shadow-lg">
-        <div className="rounded-t-2xl  text-center bg-black text-white px-32 py-8">
+      <div className=" shadow-lg rounded-b-2xl">
+        <div className="rounded-t-2xl  text-center bg-black text-white px-40 py-8">
           <h1 className="text-lg font-semibold">Musc</h1>
           <h2 className="text-3xl font-semibold">نسجيل الدخول</h2>
         </div>
-        <form className="px-16 py-8"></form>
+        <form className=" p-8 flex flex-col gap-6" dir="rtl">
+          <div>
+            <Label className="text-lg">البريد الاكتروني</Label>
+            <Input
+              type="email"
+              placeholder="البريد الاكتروني"
+              required
+              className="mt-2"
+            />
+          </div>
+          <div>
+            <Label className="text-lg">كلمة المرور </Label>
+            <Input
+              type="password"
+              placeholder=" كلمة المرور"
+              required
+              className="mt-2"
+            />
+          </div>
+          <Button>تسجيل الدخول</Button>
+        </form>
       </div>
     </div>
   );
