bbe292fba2eefc5d92027347d8940a6129b0fe80
Ahmed <ahmedabozaid.dev@gmail.com>
2025-01-10 16:14:31 +0200

    fixes loading

--- src/components/general/Loading.tsx
+++ src/components/general/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <>
-      <div className="absolute h-screen w-screen inset-0 flex items-center justify-center z-20">
+      <div className="absolute h-screen w-screen bg-white inset-0 flex items-center justify-center z-50">
         <div className="animate-pulse">
           <h3 className="font-semibold text-4xl">Musc</h3>
         </div>
